d3d02e404ac279a81702339d67c1d28195eb42ff
Zviad Kikvidze <1021281+zkikvidze@users.noreply.github.com>
2022-07-18 13:26:59 +0400

    Update AAD-Bitlocker-Backup.ps1

--- AAD-Bitlocker-Backup.ps1
+++ AAD-Bitlocker-Backup.ps1
@@ -1,3 +1,8 @@
+#AAD-Bitlocker-Backup.ps1 - Script for backing up bitlocker keys of AAD devices. 
+#Used for stale devices before deleting them from AAD (As per Microsoft Recommendation https://docs.microsoft.com/en-us/azure/active-directory/devices/manage-stale-devices).
+#Will Run only with delegated permissions.
+
+
 #required modules:
 #Microsoft.Graph.Authentication
 #Microsoft.Graph.Identity.DirectoryManagement
